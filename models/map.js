f78909078d06cda25ce511cad73a11a8f102f1ba
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2015-09-28 16:00:03 +0300

    info.json in maps

--- models/map.js
+++ models/map.js
@@ -4,6 +4,12 @@ var Schema = mongoose.Schema;
 
 var Map = new Schema({
   fileName: String,
+  name: String,
+  color: Boolean,
+  scale: Number,
+  loads: String,
+  unloads: String,
+  mileage: Number,
 });
 
 module.exports = mongoose.model('Map', Map);
--- routes/maps.js
+++ routes/maps.js
@@ -3,6 +3,7 @@ var multer  = require('multer')
 var upload = multer({ dest: 'tmp/uploads/' });
 var Map = require('../models/map');
 var fs = require('fs');
+var AdmZip = require('adm-zip');
 
 var router = express.Router();
 
@@ -13,9 +14,21 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', upload.single('map'), function(req, res){
-  fs.rename('tmp/uploads/' + req.file['filename'], 'public/maps/' + req.file['originalname'])
+  uploadedName = 'tmp/uploads/' + req.file['filename'];
+
+  var zip = new AdmZip("./seattle.zip");
+  var data = zip.readAsText(zip.getEntry('info.json'));
+  var info = JSON.parse(data)
+
+  fs.rename(uploadedName, 'public/maps/' + req.file['originalname'])
   Map.create({
     fileName: req.file['originalname'],
+    name: info['name'],
+    color: info['color'],
+    scale: info['scale'],
+    loads: info['loads'],
+    unloads: info['unloads'],
+    mileage: info['mileage'],
   });
   res.redirect('/maps');
 });
