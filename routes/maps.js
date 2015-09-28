414edd9f658d6588f1018acecf2929ee4d9db797
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2015-09-28 16:16:14 +0300

    Display maps info

--- routes/maps.js
+++ routes/maps.js
@@ -15,22 +15,30 @@ router.get('/', function(req, res, next) {
 
 router.post('/', upload.single('map'), function(req, res){
   uploadedName = 'tmp/uploads/' + req.file['filename'];
+  newName = 'public/maps/' + req.file['originalname'];
+  fs.exists(newName, function(exists) {
+    if (exists) { // TODO
+      res.render('500');
+      return;
+    }
 
-  var zip = new AdmZip("./seattle.zip");
-  var data = zip.readAsText(zip.getEntry('info.json'));
-  var info = JSON.parse(data)
+    var zip = new AdmZip(uploadedName);
+    var data = zip.readAsText(zip.getEntry('info.json'));
+    var info = JSON.parse(data);
 
-  fs.rename(uploadedName, 'public/maps/' + req.file['originalname'])
-  Map.create({
-    fileName: req.file['originalname'],
-    name: info['name'],
-    color: info['color'],
-    scale: info['scale'],
-    loads: info['loads'],
-    unloads: info['unloads'],
-    mileage: info['mileage'],
+    fs.rename(uploadedName, newName);
+    Map.create({
+      fileName: req.file['originalname'],
+      name: info['name'],
+      color: info['color'],
+      scale: info['scale'],
+      loads: info['loads'],
+      unloads: info['unloads'],
+      mileage: info['mileage'],
+    });
+    res.redirect('/maps/');
   });
-  res.redirect('/maps');
+
 });
 
 module.exports = router;
