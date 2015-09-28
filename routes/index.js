8f8bd64c8dfa4eb3543a7e47883dbc66052c6987
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2015-09-28 13:52:50 +0300

    Update page edit

--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,41 @@ var Page = require('../models/page');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  Page.find({name: 'home'}, function (err, docs) {
-    body = docs[0]['body'];
+  Page.findOne({name: 'home'}, function (err, doc) {
+    var body;
+
+    if(doc)
+      body = doc['body'];
+    else
+      body = '';
 
     res.render('index', { title: 'Home', content: body });
   });
 });
 
 router.get('/home/edit', function(req, res, next) {
-  res.render('edit-page', { page: 'home' });
+  Page.find({name: 'home'}, function (err, docs) {
+    body = docs[0]['body'];
+
+    res.render('edit-page', { page: 'home', pageContent: body});
+  });
+
 });
 
 router.post('/home/edit', function(req, res, next) {
-  Page.create({
-    name: 'home',
-    body: req.body.data
+  Page.findOne({name: 'home'}, function (err, doc) {
+    if(!doc) {
+      Page.create({
+        name: 'home',
+        body: req.body.data
+      });
+    } else {
+      doc.body = req.body.data;
+      doc.save();
+    }
+
   });
-  res.redirect('/home/edit');
+  res.redirect('/');
 });
 
 module.exports = router;
